bdf633027b1705a6615ee38059b63ead0204952b
Vivek Dave <vivekdave20@gmail.com>
2025-11-15 10:46:38 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,16 @@
+#Task 1: Perform Basic Mathematical Operations
+a= input("enter the first number : ")
+b= input("enter the second number : ")
+a=int(a)
+b=int(b)
+print("\nAddition =" ,a+b)
+print("Substraction =" ,a-b)
+print("Multiplication =" ,a*b)
+print("Division =" ,a/b)
+
+#Task 2: Create a Personalized Greeting
+
+a= input("Enter your first name:\t")
+b= input("Enter your last name:\t")
+
+print("Hello " + a +" "+ b + "! Welcome to the Python program.")
